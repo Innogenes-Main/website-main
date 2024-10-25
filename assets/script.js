73cb18570131c4cda90b51d0b77a4d221b091cd4
Quang Pham <quang4dev@gmail.com>
2024-10-25 14:32:22 +0700

    Init

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// Set the date and time you're counting down to
+var countDownDate = new Date("Nov 01, 2024 23:59:59").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  // Find the difference between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the results in the relevant HTML elements
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "0";
+    document.getElementById("hours").innerHTML = "0";
+    document.getElementById("minutes").innerHTML = "0";
+    document.getElementById("seconds").innerHTML = "0";
+  }
+}, 1000);
+screen.orientation.lock();
+$(document).ready(function () {
+  if (!window.matchMedia)
+    return;
+
+  var current = $('head > link[rel="icon"][media]');
+  $.each(current, function (i, icon) {
+    var match = window.matchMedia(icon.media);
+
+    function swap() {
+      if (match.matches) {
+        current.remove();
+        current = $(icon).appendTo('head');
+      }
+    }
+
+    match.addListener(swap);
+    swap();
+  });
+});
